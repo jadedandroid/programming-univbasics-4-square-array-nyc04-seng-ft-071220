2a4a1d9f6a49803540969b0bf55ba502ba26a395
jade decordova <lacroixjaded@gmail.com>
2020-07-16 18:07:27 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   squared = []
   counter = 0
   while counter < array.length{
-  square = array[counter] ** 2
+  square = array[counter] * 2
   squared.push(square)
   counter += 1
   }
