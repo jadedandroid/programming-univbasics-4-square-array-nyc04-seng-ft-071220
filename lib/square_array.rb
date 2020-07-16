040631607b2856590e588112bd03c78b0464cf6f
jade decordova <lacroixjaded@gmail.com>
2020-07-16 18:10:19 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,10 @@
 def square_array(array)
   squared = []
   counter = 0
-  until counter == array.length{
-  square = array[counter] * 2
-  squared << square
-  counter += 1
-  }
+  until counter == array.length
+        square = array[counter] * 2
+        squared << square
+        counter += 1
   end
   squared
 end
