eccf3ce12787442ccfc4121386dc03c63b2750fc
jade decordova <lacroixjaded@gmail.com>
2020-07-16 17:58:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,9 @@
 def square_array(array)
   squared = []
-  length= array.length
-  length.times do |index|
-    
+  counter = 0
+  while counter > array.length {
+   squared.push(array[counter] ** 2)
+   counter += 1
+  }
+  puts squared
 end
